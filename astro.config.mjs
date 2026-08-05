2206cf2ed278651eeb23c1d0de9b6d187d94d009
Jhon Mejia <johnmejiaduran@gmail.com>
2026-08-05 17:46:30 -0500

    added preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
-  site: "https://noansblog.netlify.app"
+  site: "https://noansblog.netlify.app",
+  integrations: [preact()]
 });
